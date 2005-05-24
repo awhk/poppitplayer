4af5f255f3ce0eea9f0c510a6260e458b7ee6ee5
awhenry <awhenry>
2005-05-24 03:27:48 +0000

    *** empty log message ***

--- poppitplayer/simpoppit/BalloonBits.java
+++ poppitplayer/simpoppit/BalloonBits.java
@@ -9,21 +9,26 @@ import java.util.*;
  */
 public class BalloonBits implements Comparable{
 	
-	public BalloonBits(){
-		this.bitArray = new BitSet(100);
-	}
+//	public BalloonBits(){
+//		this.bitArray = new BitSet(100);
+//	}
 	
-	public BalloonBits(int size){
-		this.bitArray = new BitSet(size);
+	public BalloonBits(int aSize){
+		this.bitArray = new BitSet(aSize);
+        this.size = aSize;
 	}
 	
 	public BalloonBits(ArrayList<Balloon> aList){
 		
 	}
 	
-	public BitSet getSet(){
+	private BitSet getSet(){
 		return this.bitArray;
 	}
+    
+    public int getSize(){
+        return this.size;
+    }
 	
 	private void setBalloon(int anIndex, Balloon aBalloon){
 		this.bitArray.clear(anIndex, anIndex+3);
@@ -31,7 +36,7 @@ public class BalloonBits implements Comparable{
 	}
 	
 	private BitSet getMask(int anIndex, Balloon aBalloon){
-		BitSet result = new BitSet(this.bitArray.length());
+		BitSet result = new BitSet(this.getSize());
 		int balloonVal = aBalloon.getNumber();
 		
 		if (balloonVal == 1 || balloonVal == 3 || balloonVal == 5){
@@ -57,13 +62,17 @@ public class BalloonBits implements Comparable{
 	
 	public int compareTo(Object aBalloonBits){
 		if (this.bitArray.equals(aBalloonBits)) return 0;
-		int val1 = 0;
-		int val2 = 0;
-		for (int i=1; i<=this.bitArray.length(); i++){
-			if (this.bitArray.get(i)) val1 += i;
-			if (((BalloonBits)aBalloonBits).getSet().get(i)) val2 += i;
-		}
-		return val1 - val2;
+//		int val1 = this.getSize();
+//		int val2 = val1;
+        this.bitArray.andNot(((BalloonBits)aBalloonBits).getSet());
+        //System.out.println("length is " + val1);
+//		for (int i=val1; i>0; i--){
+//            //System.out.println("val1=" + val1 + " , val2=" + val2);
+//			if ((this.bitArray.get(i))) val1 = val1 - i;
+//			if ((((BalloonBits)aBalloonBits).getSet().get(i))) val2 = val2 - i;
+//		}
+//		return val1 - val2;
+        return this.bitArray.length() - ((BalloonBits)aBalloonBits).getSet().length();
 	}
 	
 	public String toString(){
@@ -71,22 +80,26 @@ public class BalloonBits implements Comparable{
 	}
 	
 	private BitSet bitArray;
+    private int size;
 	
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		BalloonBits test = new BalloonBits(13);
-		Balloon test2 = new Balloon();
+		//Balloon test2 = new Balloon(Balloon.Color.PURPLE);
+        Balloon test2 = new Balloon();
 		BalloonBits test3 = new BalloonBits(13);
 		test.setBalloon(BalloonBits.getIndex(4), test2);
 		System.out.println(test2.getNumber());
-		test2 = new Balloon();
+		//test2 = new Balloon(Balloon.Color.YELLOW);
+        test2 = new Balloon();
 		test3.setBalloon(BalloonBits.getIndex(4), test2);
 		//test3.getSet().set(13);
 		//System.out.println(BalloonBits.getIndex(2));
 		System.out.println(test2.getNumber());
 		System.out.println(test);
+        System.out.println(test3);
 		System.out.println(test.compareTo(test3));
 		//System.out.println(test3.compareTo(test));
 	}

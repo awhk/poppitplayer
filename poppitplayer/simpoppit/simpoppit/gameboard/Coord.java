1175749211199555b7b808798b6c8ac160830a6b
awhenry <awhenry>
2005-06-21 15:09:15 +0000

    *** empty log message ***

--- poppitplayer/simpoppit/simpoppit/gameboard/Coord.java
+++ poppitplayer/simpoppit/simpoppit/gameboard/Coord.java
@@ -1,54 +1,82 @@
 package simpoppit.gameboard;
+
 import java.util.*;
+
 /**
- * <p>Title: Coord</p>
- *
- * <p>Description: Coordinate class</p>
- *
- * <p>Copyright: Copyright (c) 2005</p>
- *
+ * <p>
+ * Title: Coord
+ * </p>
+ * 
+ * <p>
+ * Description: This class encapsulates data and methods to allow representation
+ * and manipulation of 2-D coordinates.
+ * </p>
+ * 
+ * <p>
+ * Copyright: Copyright (c) 2005
+ * </p>
+ * 
  * @author Andrew W. Henry
  * @version 1.0
  */
-public class Coord implements Cloneable, Comparable{
-    
-    public Coord(){
-        this(0,0);
-    }
-    
-    public Coord(int aX, int aY){
+public class Coord implements Cloneable, Comparable {
+
+    /**
+     * Generate a new coordinate object with the given X and Y values
+     * @param aX the X coordinate of this point
+     * @param aY the Y coordinate of this point
+     */
+    public Coord(int aX, int aY) {
         this.x = aX;
         this.y = aY;
     }
-    
-    public int getX(){
+
+    /**
+     * Return the X coordinate of this Coord object.
+     * 
+     * @return this object's integer X coordinate
+     */
+    public int getX() {
         return x;
     }
-    
-    public int getY(){
+
+    /**
+     * Return the Y coordinate of this Coord object.
+     * 
+     * @return this object's integer Y coordinate
+     */
+    public int getY() {
         return y;
     }
-    
-    public boolean equals(Object aPoint){
-        if ((aPoint instanceof Coord) && (((Coord)aPoint).getX() == this.x & ((Coord)aPoint).getY() == this.y) ) return true;
+
+    /* (non-Javadoc)
+     * @see java.lang.Object#equals(java.lang.Object)
+     */
+    public boolean equals(Object aPoint) {
+        if ((aPoint instanceof Coord)
+                && (((Coord) aPoint).getX() == this.x & ((Coord) aPoint).getY() == this.y))
+            return true;
         return false;
     }
-    
-    public int compareTo(Object aPoint){
+
+    /* (non-Javadoc)
+     * @see java.lang.Comparable
+     */
+    public int compareTo(Object aPoint) {
         int result = 0;
-        result = this.x - ((Coord)aPoint).getX();
-        if (result == 0){
-                result = this.y - ((Coord)aPoint).getY();
+        result = this.x - ((Coord) aPoint).getX();
+        if (result == 0) {
+            result = this.y - ((Coord) aPoint).getY();
         }
         return result;
     }
-    
-    public Object clone(){
+
+    public Object clone() {
         Coord result = new Coord(this.x, this.y);
         return result;
     }
-    
-    public String toString(){
+
+    public String toString() {
         String result = "";
         result += "(";
         result += this.x;
@@ -58,16 +86,16 @@ public class Coord implements Cloneable, Comparable{
         return result;
     }
 
-    
     private int x;
+
     private int y;
-    
+
     /**
      * @param args
      */
     public static void main(String[] args) {
-        Coord test1 = new Coord(3,3);
-        Coord test2 = new Coord(3,3);
+        Coord test1 = new Coord(3, 3);
+        Coord test2 = new Coord(3, 3);
         ArrayList<Coord> testlist = new ArrayList<Coord>();
         testlist.add(test1);
         System.out.print("test1 = test2 is ");

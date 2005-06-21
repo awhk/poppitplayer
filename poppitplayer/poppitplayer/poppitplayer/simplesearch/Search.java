c72e53753f2ec3ed0dc51fcec327b9ad5cd47e2a
awhenry <awhenry>
2005-06-21 14:47:52 +0000

    *** empty log message ***

--- poppitplayer/poppitplayer/poppitplayer/simplesearch/Search.java
+++ poppitplayer/poppitplayer/poppitplayer/simplesearch/Search.java
@@ -180,17 +180,27 @@ public abstract class Search {
                         break;
                     }
                 }
-                // Since we found a solution, but not a perfect one, clear our
-                // loop/node counter and begin searching again
+                // If we get here then we found a solution, but not a perfect
+                // one, so clear our loop/node counter and begin searching again
                 loopCount = 0;
-//                break;
+                // A break here would stop after finding any solution
+                // break;
             }
+            // Store the current node in our "visited nodes" store
             this.storeSeen(this.node);
+            // If the current node is a solution node, then it cannot be
+            // expanded because there would by definition be no more moves left.
+            // So check first to see if the current node is a solution node.
             if (!(this.goalState())) {
+                // If this is not a solution node then expand it
                 this.expand();
             }
+            // Get the next available search node
             this.nextNode();
         }
+        // If we get here, we have exhausted the search space, so we will output
+        // a little summary noting that along with details about the best
+        // solution we did find.
         if ((!this.perfect) && (this.bestNode != null)) {
             System.out.println("\nFound solution after examining " + loopCount
                     + " nodes.");
@@ -208,127 +218,281 @@ public abstract class Search {
         }
     }
 
+    /**
+     * This method will play back the game solution contained in the bestNode
+     * variable (i.e. the best solution discovered by the search method). This
+     * method presumes that the search method has been run prior to this. This
+     * method employs the human-player gui interface for output.
+     */
     public void playbackSolution() {
-        // s_runtime.runFinalization();
-        // s_runtime.gc();
-        // Thread.yield();
+        // If the bestNode variable is undefined, something is really wrong so
+        // abort the playback
+        if (this.bestNode == null)
+            return;
+        // First, initialize a "node pointer" to the ultimate/solution node
         SearchNode myNode = this.bestNode;
-        // SearchNode myNode = this.node;
+        // Initialize a stack to hold the move list. Moves will be extracted
+        // from the solution sequence in reverse order, so a stack will allow
+        // them to be popped off and the game replayed from start to finish.
         Stack<Coord> moves = new Stack<Coord>();
-        // GameInterface myGame = gui.getGame();
+        // Iterate through the nodes of the solution set, starting from the last
+        // node and ending at the first.
         while (myNode.getAncestor() != null) {
+            // Retrieve the operator from the current search node (i.e. the move
+            // that resulted in the game state of the current node)
             moves.push(myNode.getOperator());
+            // Prime the next node up in the game solution sequence for
+            // examination
             myNode = myNode.getAncestor();
         }
+        // Initialize the gui with a COPY of the starting game state, so the
+        // replay can be run more than once. Start the gui in non-interactive
+        // mode.
         SimPoppitGui gui = new SimPoppitGui((GameInterface) myNode.getState()
                 .clone(), false);
         gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         gui.setVisible(true);
-        // System.out.println("Number of moves is " + moves.size());
+        // Put up a dialog box to allow the start of the playback to be delayed
+        // until the viewer is ready
         JOptionPane.showMessageDialog(gui, "Click OK to watch playback",
                 "Game Solved", JOptionPane.DEFAULT_OPTION);
+        // Iterate through the move stack
         while (!(moves.isEmpty())) {
+            // Highlight the impending move...
             gui.getGame().highlight(moves.peek());
+            // ...for one second...
             try {
                 Thread.sleep(1000);
             } catch (Exception e) {
                 System.out.println("Failed to sleep - " + e);
             }
+            // ...then make the move.
             gui.getGame().pop(moves.pop());
         }
+        // When the playback is complete, offer the option to watch it again
         int selection = JOptionPane.showConfirmDialog(gui,
                 "Click OK to watch playback again or cancel not to",
                 "Game Solved", JOptionPane.OK_CANCEL_OPTION);
+        // If the user does not want to see it again, just wait, displaying the
+        // solution game layout
         if (selection == JOptionPane.CANCEL_OPTION) {
-            // System.exit(0);
         } else {
+            // Otherwise, annihilate the existing gui instance
             gui.setVisible(false);
             gui.dispose();
             gui = null;
+            // And start the playback process from scratch
             this.playbackSolution();
         }
     }
 
+    /**
+     * Return the total number of solutions found so far by the current search
+     * 
+     * @return integer number of solutions found
+     */
     public int getSolutionTotal() {
         return this.solutionsFound;
     }
 
+    /**
+     * Return the score of the best solution found so far by the current search.
+     * 
+     * @return integer score of best solution so far
+     */
     public int getBestScore() {
         return this.bestScore;
     }
 
+    /**
+     * Return the number of nodes that were generated as successors to some node
+     * under consideration that were discarded because they duplicated nodes
+     * that had previously been examined. I.e., this is the number of game
+     * states encountered so far that are basically a different way to get to
+     * the same place, and we have already gotten to that place once already.
+     * 
+     * @return integer number of nodes discarded due to previous node
+     *         duplication
+     */
     public int getSkippedSeen() {
         return this.skippedBecauseSeen;
     }
 
+    /**
+     * Return the number of nodes that were generated as successors to some node
+     * under consideration that were discarded because they duplicated nodes
+     * that were already scheduled to be examined.
+     * 
+     * @return integer number of nodes discarded due to pending node duplication
+     */
     public int getSkippedUnseen() {
         return this.skippedBecauseUnseen;
     }
 
+    /**
+     * Add a {@link SearchNode} to the store of nodes that have been visited.
+     * 
+     * @param aNode
+     *            a visited (seen) node
+     */
     public void storeSeen(SearchNode aNode) {
         this.seenNodes.add(aNode);
     }
 
-    public boolean seenEmpty() {
-        return this.seenNodes.isEmpty();
-    }
-
+    /**
+     * Return the number of nodes contained in the visited/seen nodes store.
+     * 
+     * @return integer number of nodes already visited
+     */
     public int seenSize() {
         return this.seenNodes.size();
     }
 
+    /**
+     * Return true if the given node already exists in the store of nodes that
+     * have been seen/visited.
+     * 
+     * @param aNode
+     *            a node to test for inclusion in the "visited" store
+     * @return boolean
+     */
     public boolean seenContains(SearchNode aNode) {
         return this.seenNodes.contains(aNode);
     }
 
+    /**
+     * Add the given node both to the local store of unseen/unvisited nodes as
+     * well as the implementation-specific store (i.e. the store that defines
+     * the behavior of the search).
+     * 
+     * @param aNode
+     *            the unseen/unvisited node to store
+     */
     public void storeUnseen(SearchNode aNode) {
+        // Add the node to the local store
         this.unseenNodes.add(aNode);
-        this.enqueueUnseen(aNode);
+        // Add the node to the implementation-specific store
+        enqueueUnseen(aNode);
     }
 
+    /**
+     * Return true if the store of unseen/unvisted nodes is empty, false
+     * otherwise.
+     * 
+     * @return boolean
+     */
     public boolean unseenEmpty() {
         return this.unseenNodes.isEmpty();
     }
 
+    /**
+     * Return the integer number of nodes that have been generated so far by
+     * {@link #expand()}ing other nodes, but that have not yet been visited
+     * themselves.
+     * 
+     * @return number of nodes generated but not visited yet
+     */
     public int unseenSize() {
         return this.unseenNodes.size();
     }
 
+    /**
+     * Return true if the given node already exists in the store of nodes that
+     * have not yet been seen/visited.
+     * 
+     * @param aNode
+     *            a node to test for inclusion in the unseen/unvisited store
+     * @return boolean
+     */
     public boolean unseenContains(SearchNode aNode) {
         return this.unseenNodes.contains(aNode);
     }
 
+    /**
+     * Remove the given node from the local store of unseen/unvisited nodes.
+     * This is done whenever a node is removed fromt he implementation-dependent
+     * store to keep the two stores synchronized.
+     * 
+     * @param aNode
+     *            the node to remove fromt he local store
+     */
     public void unseenDrop(SearchNode aNode) {
         this.unseenNodes.remove(aNode);
     }
 
+    /**
+     * Add a node to the implementation-dependent store. The nature of the
+     * implementation of this method will affect the type of search that is
+     * ultimately performed by this class.
+     * 
+     * @param aNode
+     *            an unseen/unvisited node to be stored for future analysis
+     */
     abstract public void enqueueUnseen(SearchNode aNode);
 
+    /**
+     * Remove a node from the implementation-dependent store. The nature of the
+     * implementation of this method will affect the type of search that is
+     * ultimately performed by this class.
+     * 
+     * @return the "next" node to be tested as determined by the implementor of
+     *         this method
+     */
     abstract public SearchNode dequeueUnseen();
 
+    /**
+     * Returns the number of nodes currently in the implementation-dependent store.
+     * 
+     * @return integer number of unseen nodes currently in storage
+     */
     abstract public int queueSize();
 
+    /**
+     * Node currently being examined
+     */
     protected SearchNode node;
 
+    /**
+     * Node representing best game solution so far
+     */
     protected SearchNode bestNode = null;
 
+    /**
+     * Score of best game so far
+     */
     protected int bestScore = 0;
 
+    /**
+     * Number of solutions found so far
+     */
     protected int solutionsFound = 0;
 
+    /**
+     * Number of nodes skipped because they duplicate visited nodes
+     */
     protected int skippedBecauseSeen = 0;
 
+    /**
+     * Number of nodes skipped because they duplicate unvisited nodes
+     */
     protected int skippedBecauseUnseen = 0;
 
+    /**
+     * Boolean switch to short-circuit search if a perfect game is found
+     */
     protected boolean perfect = false;
 
+    /**
+     * Local storage for visited nodes
+     */
     protected TreeSet<SearchNode> seenNodes = new TreeSet<SearchNode>();
 
+    /**
+     * Local storage for unvisited nodes
+     */
     protected TreeSet<SearchNode> unseenNodes = new TreeSet<SearchNode>();
 
     // public static Logger log = Logger.getLogger(Search.class);
-
-    // private static final Runtime s_runtime = Runtime.getRuntime();
 
     public static void main(String[] args) {
         GameInterface game = new GameInterface(10, 10);
